29f861e35a4bb057c7ec733f97167e1410a8c23b
thiago <thenrique2012@gmail.com>
2021-01-03 15:20:45 -0300

    sass configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  "stories": ["../packages/**/stories/*.stories.tsx"],
+  "stories": ["../src/packages/**/stories/*.stories.(mdx|tsx)"],
   "addons": [
+    '@storybook/preset-scss',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
   ],
   webpackFinal: async (config, {configType}) => {
     config.resolve.modules = [
       ...(config.resolve.modules || []),
-      path.resolve('./'),
-    ]
+      path.resolve('./src'),
+    ];
 
     return config;
   }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import '!style-loader!css-loader!sass-loader!../src/sass/_app.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
